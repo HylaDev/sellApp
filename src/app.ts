f4825923eddfda6bcf5f89984004d10c044b7b49
HylaDev <ernestdossa.9@gmail.com>
2025-01-10 15:44:28 +0100

    calcul simple du prix total

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,23 @@
+type Product = {
+    quantity: number,
+    price: number,
+    etat?: etat
+}
+
+type etat = "UT" | "NV" | "TX" | "AL" | "CA"
+
+type Taxe = {
+    etat: etat,
+    tax: number
+}
+
+function calculTotal(product: Product): number {
+    return product.price * product.quantity
+}
+
+
 console.log("Bienvenue sur votre application de vente")
-const price: number = 6
-const quantity: number = 10
-console.log( "Quantité: ", quantity, "Prix: ", price)
-console.log( "Total: ", price * quantity)
+
+
+console.log( "Quantité: ", 10, "Prix: ", 6)
+console.log( "Total: ", calculTotal({quantity: 10, price: 6}))
