bfc86c7e55502c53eab2436aee0b57bbd97533aa
HylaDev <ernestdossa.9@gmail.com>
2025-01-10 15:35:20 +0100

    first push

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Bienvenue sur votre application de vente");
+const price = 6;
+const quantity = 10;
+console.log("Quantité: ", quantity, "Prix: ", price);
+console.log("Total: ", price * quantity);
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+console.log("Bienvenue sur votre application de vente")
+const price: number = 6
+const quantity: number = 10
+console.log( "Quantité: ", quantity, "Prix: ", price)
+console.log( "Total: ", price * quantity)
